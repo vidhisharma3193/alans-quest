ad82426abede20f396873dc118e4eec75d15ca4e
jwfrasier <frasier.joseph@gmail.com>
2019-01-08 11:42:28 -0600

    updated for more generic wording

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const alan = {
-     ask: function(question, answer){
-        console.log(question)
-        alan.responds = answer
-    }
-}
+const student = {
+  ask: function(question, answer) {
+    console.log(question);
+    student.responds = answer;
+  }
+};
+
+student.ask("Staying in bed?", function(answer) {});
